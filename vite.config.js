eeb0e9e1dc62b57a72724a25477faef7e5ab84e9
Adora <im.stariluz@gmail.com>
2025-05-21 23:44:14 -0600

    Deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
+import { fileURLToPath, URL } from 'node:url'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ }) => {
+  console.log("VITE_REPO_NAME:", process.env.VITE_REPO_NAME);
+  return {
+    plugins: [react()],
+
+    resolve: {
+      alias: {
+        'src': fileURLToPath(new URL('./src', import.meta.url))
+      },
+    },
+
+    base: process.env.NODE_ENV === 'production'
+      ? `${process.env.VITE_REPO_NAME}` || '/'
+      : '/',
+  }
 })
